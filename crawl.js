ef3778d0a5e23f6976ea6778755c97d94a9c20b9
ddang9390 <dnld1ng@yahoo.com>
2024-05-17 16:16:33 -0400

    Initial crawl function

--- crawl.js
+++ crawl.js
@@ -1,6 +1,6 @@
 import {JSDOM} from 'jsdom';
 
-export { normalizeURL, getURLsFromHTML };
+export { normalizeURL, getURLsFromHTML, crawlPage };
 
 function normalizeURL(url){
     const myURL = new URL(url); 
@@ -23,4 +23,53 @@ function getURLsFromHTML(htmlBody, baseURL){
     });
 
     return URLs;
+}
+
+function crawlPage(url){
+    fetch(url).then(resp =>{
+        if (resp.status >= 400){
+            console.log("Could not access page");
+            return;
+        }
+        if (resp.headers.get("content-type").split(";")[0] != "text/html"){
+           console.log("Could not read contents");
+           return;
+        }
+        return resp.body;
+    }).then((rb) => {
+        const reader = rb.getReader();
+
+        return new ReadableStream({
+        start(controller) {
+            // The following function handles each data chunk
+            function push() {
+            // "done" is a Boolean and value a "Uint8Array"
+            reader.read().then(({ done, value }) => {
+                // If there is no more data to read
+                if (done) {
+                console.log("done", done);
+                controller.close();
+                return;
+                }
+                // Get the data and send it to the browser via the controller
+                controller.enqueue(value);
+                // Check chunks by logging to the console
+                console.log(done, value);
+                push();
+            });
+            }
+
+            push();
+        },
+        });
+    })
+    .then((stream) =>
+        // Respond with our stream
+        new Response(stream, { headers: { "Content-Type": "text/html" } }).text(),
+    )
+    .then((result) => {
+        // Do things with result
+        console.log(result);
+    });
+
 }
--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+import { crawlPage } from "./crawl.js";
+
+function main(){
+    const args = process.argv.slice(2);
+
+    if (args.length < 1 || args.length > 1){
+        console.log("Please include just one argument");
+    }
+    else{
+        args.forEach(val => {
+            crawlPage(val);
+
+        });
+    }
+}
+
+main();
